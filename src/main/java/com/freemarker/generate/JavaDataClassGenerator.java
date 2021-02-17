81b0912e6ba1c19a2d1cb8751446eec1ab739fad
Gleb Sytnik <gleb.sytnik@gmail.com>
2021-02-17 11:56:13 +0200

    second commit

--- src/main/java/com/freemarker/generate/JavaDataClassGenerator.java
+++ src/main/java/com/freemarker/generate/JavaDataClassGenerator.java
@@ -13,38 +13,26 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
-public class JavaDataClassGenerator {	
-	private Configuration configuration;
+public class JavaDataClassGenerator {
+    private Configuration configuration;
 
-	public JavaDataClassGenerator() throws IOException {		
-		configuration = new Configuration(Configuration.VERSION_2_3_28);
-		
-		// Set the root of the class path ("") as the location to find templates
-		configuration.setClassLoaderForTemplateLoading(getClass().getClassLoader(), "");
-		
-		configuration.setDefaultEncoding("UTF-8");
-		configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-		configuration.setLogTemplateExceptions(false);
-		configuration.setWrapUncheckedExceptions(true);
-	}
+    public JavaDataClassGenerator() throws IOException {
+        configuration = new Configuration(Configuration.VERSION_2_3_28);
+        configuration.setClassLoaderForTemplateLoading(getClass().getClassLoader(), "");
+        configuration.setDefaultEncoding("UTF-8");
+        configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        configuration.setLogTemplateExceptions(false);
+        configuration.setWrapUncheckedExceptions(true);
+    }
 
-	public void generateJavaSourceFiles(Collection<MetaDataInfo> metaDataInfos, File fileDirectory) throws Exception {
-		Map<String, Object> freemarkerDataModel = new HashMap<>();
-		
-		// Get the template to generate Java source files
-		Template template = configuration.getTemplate("javadataclass.ftl");
-		
-		for (MetaDataInfo metaDataInfo : metaDataInfos) {
-			// Put the classSpecification into the data model.
-			// It can  be accessed in the template through ${classSpecification}
-			freemarkerDataModel.put("metaDataInfo", metaDataInfo);
-			
-			// The Java source file will be generated in the same directory as the YAML file
-			File javaSourceFile = new File(fileDirectory, metaDataInfo.getName() + ".java");
-			Writer javaSourceFileWriter = new FileWriter(javaSourceFile);
-			
-			// Generate the Java source file
-			template.process(freemarkerDataModel, javaSourceFileWriter);
-		}
-	}
+    public void generateJavaSourceFiles(Collection<MetaDataInfo> metaDataInfos, File fileDirectory) throws Exception {
+        Map<String, Object> freemarkerDataModel = new HashMap<>();
+        Template template = configuration.getTemplate("javadataclass.ftl");
+        for (MetaDataInfo metaDataInfo : metaDataInfos) {
+            freemarkerDataModel.put("metaDataInfo", metaDataInfo);
+            File javaSourceFile = new File(fileDirectory, metaDataInfo.getName() + ".java");
+            Writer javaSourceFileWriter = new FileWriter(javaSourceFile);
+            template.process(freemarkerDataModel, javaSourceFileWriter);
+        }
+    }
 }
